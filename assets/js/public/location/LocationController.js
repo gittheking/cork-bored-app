4e09e49eef05445478ca28abd07da0df05398af1
Bobby King <king.robertjoseph@gmail.com>
2015-07-16 17:19:29 -0400

    Adding cork background

--- assets/js/public/location/LocationController.js
+++ assets/js/public/location/LocationController.js
@@ -1,5 +1,6 @@
 angular.module('LocationModule').controller('LocationController', [ '$scope', function($scope){
 	$scope.submitLocationForm = function(){
 		console.log($scope.locationForm.region);
+		$scope.locationForm.region = '';
 	}
 }])
